bc764ba0b213dd9fc3940d2526c6e1494f1ec635
raed bahri <raedbahri90@gmail.com>
2022-02-27 13:39:23 +0100

    add search

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
 	const [Data, setData] = useState([]);
+	const [InputValue, setInputValue] = useState({});
 	useEffect(() => {
 		const getRandomPhotos = async () => {
 			const res = await axios.get(
@@ -13,16 +14,33 @@ const Home = () => {
 		};
 		getRandomPhotos();
 	}, []);
+
+	const searchImage = async (e) => {
+		e.preventDefault();
+		const res = await axios.get(
+			`https://api.unsplash.com/search/photos?query=${InputValue.search}&client_id=aCxPENt02AkEWKUExQ8UnI6jaCAL3oZz7tWNq52eXc0`
+		);
+		setData(res.data.results);
+	};
 	return (
 		<div>
 			<div className="input-form">
-				<input type="text" />
-				<button type="button">Search</button>
+				<input
+					name="search"
+					onChange={(e) => {
+						setInputValue({...InputValue, [e.target.name]: e.target.value });
+						console.log(InputValue);
+					}}
+					type="text"
+				/>
+				<button onClick={(e) => searchImage(e)} type="button">
+					Search
+				</button>
 			</div>
 			<div className="grid-container">
 				{Data.map((img) => {
 					return (
-						<Link to={`/${img.id}`}>
+						<Link key={img.id} to={`/${img.id}`}>
 							<div>
 								<img className="grid-item" src={img.urls.small} alt="" />
 								<p>"I'm so happy today!"</p>
